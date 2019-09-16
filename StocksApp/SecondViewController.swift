54e55ef3178414369e67a541d3430a70ff249193
Nahir Sanchez <ing.nahir.sanchez@gmail.com>
2019-09-16 11:12:42 -0500

    Initial Commit

---
+++ StocksApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  StocksApp
+//
+//  Created by Nahir Gamaliel Haro Sanchez on 9/16/19.
+//  Copyright © 2019 Nahir Gamaliel Haro Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
