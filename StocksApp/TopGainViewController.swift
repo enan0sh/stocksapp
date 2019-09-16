2bff30a948baee2a36935095654686e1f82905f2
Nahir Sanchez <ing.nahir.sanchez@gmail.com>
2019-09-16 18:11:18 -0500

    Deleted default view controller, created my own for top gain view

---
+++ StocksApp/TopGainViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TopGainViewController.swift
+//  StocksApp
+//
+//  Created by Nahir Gamaliel Haro Sanchez on 9/16/19.
+//  Copyright © 2019 Nahir Gamaliel Haro Sanchez. All rights reserved.
+//
+
+import Foundation
